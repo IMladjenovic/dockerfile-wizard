092a26db2cd1071f6fe4b0b6d223196c2a380676
Ivan Mladjenovic <imladjenovic@scottlogic.com>
2019-11-28 10:48:54 +0000

    changed build tag style

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ IMAGE_NAME="terraform-aws"
 TAG="0.11.14"
 DOCKER_USERNAME=imladjenovic
 
-docker build -t ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG} . && \
-docker build -t ${DOCKER_USERNAME}/${IMAGE_NAME}:latest . && \
+docker build -t ${IMAGE_NAME} . && \
+docker tag ${IMAGE_NAME} ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG} && \
+docker tag ${IMAGE_NAME} ${DOCKER_USERNAME}/${IMAGE_NAME}:latest && \
 docker push ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG} && \
 docker push ${DOCKER_USERNAME}/${IMAGE_NAME}:latest
