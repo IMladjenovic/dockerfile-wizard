eefb234859b76778be5372303d97afa068256af8
Ivan Mladjenovic <imladjenovic@scottlogic.com>
2019-11-28 10:07:31 +0000

    added my user

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ DOCKER_TAG="0.11.14"
 DOCKER_HUB_USERNAME=imladjenovic
 
 docker build -t ${DOCKER_NAME} . && \
-docker tag ${DOCKER_NAME} ${DOCKER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
-docker tag ${DOCKER_NAME} ${DOCKER_NAME}/${DOCKER_NAME}:latest && \
-docker push ${DOCKER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
-docker push ${DOCKER_NAME}/${DOCKER_NAME}:latest
+docker tag ${DOCKER_NAME} ${DOCKER_HUB_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
+docker tag ${DOCKER_NAME} ${DOCKER_HUB_USERNAME}/${DOCKER_NAME}:latest && \
+docker push ${DOCKER_HUB_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
+docker push ${DOCKER_HUB_USERNAME}/${DOCKER_NAME}:latest
