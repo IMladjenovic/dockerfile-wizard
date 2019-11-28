b8962ca3e85e8436447790b0b8721aa6aea2f95d
Ivan Mladjenovic <imladjenovic@scottlogic.com>
2019-11-28 10:49:42 +0000

    corrected username

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 IMAGE_NAME="terraform-aws"
 TAG="0.11.14"
-DOCKER_USERNAME=imladjenovic
+DOCKER_USERNAME=ivanmladjenovic
 
 docker build -t ${IMAGE_NAME} . && \
 docker tag ${IMAGE_NAME} ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG} && \
