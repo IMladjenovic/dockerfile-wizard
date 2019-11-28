b973537d4ca2df6a2b228bacd0b82c20d44f00f0
Ivan Mladjenovic <imladjenovic@scottlogic.com>
2019-11-28 10:05:07 +0000

    added my user

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 DOCKER_NAME="terraform-aws"
 DOCKER_TAG="0.11.14"
+DOCKER_HUB_USERNAME=imladjenovic
 
 docker build -t ${DOCKER_NAME} . && \
-docker tag ${DOCKER_NAME} basile/${DOCKER_NAME}:${DOCKER_TAG} && \
-docker tag ${DOCKER_NAME} basile/${DOCKER_NAME}:latest && \
-docker push basile/${DOCKER_NAME}:${DOCKER_TAG} && \
-docker push basile/${DOCKER_NAME}:latest
+docker tag ${DOCKER_NAME} ${DOCKER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
+docker tag ${DOCKER_NAME} ${DOCKER_NAME}/${DOCKER_NAME}:latest && \
+docker push ${DOCKER_NAME}/${DOCKER_NAME}:${DOCKER_TAG} && \
+docker push ${DOCKER_NAME}/${DOCKER_NAME}:latest
